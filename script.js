071647f0350d74acfa13ebba4e59a5e075731f21
unknown <podkanski050@gmail.com>
2023-08-19 14:44:45 +0200

    I typed describes

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const temperature = document.querySelector(".temperature");
 const feelsLike = document.querySelector(".feels-like");
 const humidity = document.querySelector(".humidity");
 
+// parts of URL
 const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q=";
 const API_KEY = "&appid=182b832768d412741a66b58f9eef69b9";
 const API_UNITS = "&units=metric";
@@ -18,6 +19,7 @@ const checkTemp = () => {
 	const city = input.value;
 	const URL = API_LINK + city + API_UNITS + LANG + API_KEY;
 	console.log(URL);
+	//fetch send a question to the website and if response is correctly, received a data or execute catch to inform about issue
 	fetch(URL)
 		.then(res => res.json())
 		.then(res => {
@@ -35,7 +37,7 @@ const checkTemp = () => {
 
 			warning.textContent = "";
 			input.value = "";
-
+// by means of this, it checks id of the weather and changes to a matching image
 			if (status.id >= 200 && status.id <= 232) {
 				//another idea is image.setAttribute("src", "./img/weather.svg");
 				image.src = "./img/thunder.svg";
@@ -55,6 +57,8 @@ const checkTemp = () => {
 		})
 		.catch(() => (warning.textContent = "Type correct city name!"));
 };
+
+// function indicates click on enter
 const enterClick = e => {
 	if (e.key === "Enter") {
 		checkTemp();
